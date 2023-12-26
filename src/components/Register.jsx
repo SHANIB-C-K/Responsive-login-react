d2de1b5c0be505bd3dd83564dedf273a066166f0
SHANIB C K <ckshanib8@gmail.com>
2023-12-26 11:02:54 +0530

    width andf height added at tailwind.config.js file

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register form</div>;
+  return (
+    <>
+      <div className="bg-gray-900 h-screen w-screen relative overflow-hidden">
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "40r": "40rem",
+        "35r": "30rem",
+      },
+      height: {
+        "40r": "40rem",
+        "35r": "30rem",
+      },
+    },
   },
   plugins: [],
 };
