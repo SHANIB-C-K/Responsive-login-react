8ca647b18ac66edfc0d2d587a92a864b4747a502
SHANIB C K <ckshanib8@gmail.com>
2023-12-26 11:51:20 +0530

    Second rounded ball created at Register.jsx file

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -5,6 +5,7 @@ const Register = () => {
     <>
       <div className="bg-gray-900 h-screen w-screen relative overflow-hidden">
         <div className="h-40-r w-40-r bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
+        <div className="h-35-r w-35-r bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full absolute top-96 -left-20 transform rotate-180 animate-pulse"></div>
       </div>
     </>
   );
