7f674b8df58c209e44ec8e27be0d8ae9586948d1
SHANIB C K <ckshanib8@gmail.com>
2023-12-26 12:01:06 +0530

    glass effect div created at register.jsx file

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const Register = () => {
   return (
     <>
-      <div className="bg-gray-900 h-screen w-screen relative overflow-hidden">
+      <div className="bg-gray-900 h-screen w-screen relative overflow-hidden flex items-center justify-center">
         <div className="h-40-r w-40-r bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
         <div className="h-35-r w-35-r bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full absolute top-96 -left-20 transform rotate-180 animate-pulse"></div>
+        <div className="container h-96 w-96 bg-white bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm"></div>
       </div>
     </>
   );
