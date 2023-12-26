d0b5b2abb275bfe70020759971e71b47c0e5161b
SHANIB C K <ckshanib8@gmail.com>
2023-12-26 11:41:17 +0530

    Firat rounded ball 😉

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,7 +4,7 @@ const Register = () => {
   return (
     <>
       <div className="bg-gray-900 h-screen w-screen relative overflow-hidden">
-        <div className="h-[40rem] w-[40rem] bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
+        <div className="h-40-r w-40-r bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
       </div>
     </>
   );
