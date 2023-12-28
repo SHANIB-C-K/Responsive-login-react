fef5b10f3ee81d84091499c9515f6199107a388e
SHANIB C K <ckshanib8@gmail.com>
2023-12-28 09:55:37 +0530

    Commands added at all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+// This is importing section
 import React from "react";
 import Register from "./components/Register";
 
 const App = () => {
   return (
     <>
+      {/* This is Register page  */}
       <Register />
     </>
   );
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,13 +1,20 @@
+// This is register page
+
+// This is importing section
 import React, { useState } from "react";
 import Tilt from "react-parallax-tilt";
 
 const Register = () => {
+  // This is a useState created
   const [isLogedIn, setIsLogedIn] = useState(false);
+  // initialy its false
+
   return (
     <>
       <div className="bg-gray-900 h-screen w-screen relative overflow-hidden flex items-center justify-center">
         <div className="h-40-r w-40-r bg-gradient-to-r from-green-400 to-blue-500 rounded-full absolute left-2/3 -top-56 transform rotate-160 animate-pulse"></div>
         <div className="h-35-r w-35-r bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-full absolute top-96 -left-20 transform rotate-180 animate-pulse"></div>
+        {/* Its hover animation section */}
         <Tilt>
           <div className="container h-96 w-96 bg-white bg-opacity-10 rounded-2xl shadow-5xl relative z-2 border border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-sm">
             <form className="h-full w-full flex flex-col justify-evenly items-center">
